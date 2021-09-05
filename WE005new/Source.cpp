ff3def395b0daf8d5e2ae9cba8cbb8e6d313d0a3
DrPawat <88647474+DrPawat@users.noreply.github.com>
2021-09-05 18:09:59 +0700

    Update Source.cpp

--- WE005new/Source.cpp
+++ WE005new/Source.cpp
@@ -2,7 +2,7 @@
 int main() {
     int n, i;
     int fact = 1;
-    printf("Enter an your factorial: ");
+    printf("Enter your factorial: ");
     scanf_s("%d", &n);
     if (n < 0)
         printf("Error!");
